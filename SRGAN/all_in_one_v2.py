869716476964b69d9df5137345b3235cd1bbb939
harrier999 <harrier999@naver.com>
2022-06-18 23:34:17 +0900

    first

---
+++ SRGAN/all_in_one_v2.py
@@ -0,0 +1,46 @@
+import time
+import cv2
+import torch
+from PIL import Image
+from torch.autograd import Variable
+from torchvision.transforms import ToTensor, ToPILImage
+from model import Generator
+
+def denoise_and_upscale(IMAGE_NAME, UPSCALE_FACTOR = 4, DENOISE_STRENGTH = 2, TEST_MODE = True, MODEL_NAME = 'netG_epoch_4_100.pth'):
+    
+    #model
+    model = Generator(UPSCALE_FACTOR).eval()
+    if TEST_MODE:
+        model.cuda()
+        model.cpu()
+        model.load_state_dict(torch.load('inference_model/' + MODEL_NAME)) #모델 경로
+    else:
+        model.load_state_dict(torch.load('inference_model/' + MODEL_NAME, map_location=lambda storage, loc: storage))
+
+    #denoise
+    path = './inference_input_image/' + IMAGE_NAME 
+
+    img = cv2.imread(path, cv2.IMREAD_COLOR)
+    dst = cv2.fastNlMeansDenoisingColored(img,None,DENOISE_STRENGTH,DENOISE_STRENGTH,7,21)
+
+    temp_image = './inference_de_image/' + 'de_' + str(DENOISE_STRENGTH) + '_' + IMAGE_NAME
+
+    cv2.imwrite(temp_image, dst)
+
+    #upscale
+    image = Image.open(temp_image)
+    image = Variable(ToTensor()(image), volatile=True).unsqueeze(0)
+    if TEST_MODE:
+        image = image.cuda()
+        pass
+
+    start = time.perf_counter()
+    out = model(image)
+    elapsed = (time.perf_counter() - start)
+    print('cost' + str(elapsed) + 's')
+    out_img = ToPILImage()(out[0].data.cpu())
+    out_img.save("./inference_output_image/" + 'out_srf_' + str(UPSCALE_FACTOR) + '_' + IMAGE_NAME)
+
+    return out_img
+
+#denoise_and_upscale("Cat_Ori.jpg")
---
+++ SRGAN/main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, File
+import uvicorn
+from starlette.responses import Response
+import io
+from PIL import Image
+
+from all_in_one_v2 import denoise_and_upscale
+import cv2
+
+
+app = FastAPI()
+
+
+@app.post("/")
+def read_root():
+	'''
+	backend의 root 경로로 request가 들어올 시 dict를 return
+	Parameters: None
+	return : 
+	dict(dtype=dict) : name과 test 이미지 경로가 들어있는 dict를 반환. 
+ 	({"name"(dtype=str): "Backend/img_test"(dtype=str)}
+  	
+   	※현재 demo test를 위해 img 파일의 형식을 img_test로 강제로 바꿔서 추가했으며 이후 이 부분은 삭제될 예정 
+	'''
+	return {"name": "Backend/img_test"}
+
+
+@app.post("/getSimilarFashion")
+def get_similar_fashion(file: bytes = File(...)):
+	img = io.BytesIO(file)
+	img = Image.open(img)
+	img = img.convert("RGB")
+	img.save('./inference_input_image/' + 'test_image.jpg')
+	image = denoise_and_upscale(IMAGE_NAME='test_image.jpg', UPSCALE_FACTOR = 4, DENOISE_STRENGTH = 2, TEST_MODE = True, MODEL_NAME = 'netG_epoch_4_100.pth')
+	
+	bytes_io = io.BytesIO()
+	img.save(bytes_io, format="PNG")
+
+	return Response(bytes_io.getvalue(), media_type="image/png")
+	
+	
+
+
+
+
+# 앞으로 기능이 추가되면 사용될 예시
+# @app.get("/users/{user_id}")
+# def get_user(user_id):
+#     return {"user_id":user_id}
+
+if __name__ == '__main__':
+	uvicorn.run("main:app", host="0.0.0.0", port = 8000, reload=True) 
